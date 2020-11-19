a7cf3d7d55fdff0e6811a8f6ef012b9d83ac31f7
Leandro Freire <leandro.freire.work@gmail.com>
2020-11-19 13:11:51 -0300

    add background lottie and logo animation

--- app/src/main/java/com/github/grupo7dh/filmix/view/SpashActivity.kt
+++ app/src/main/java/com/github/grupo7dh/filmix/view/SpashActivity.kt
@@ -3,15 +3,21 @@ package com.github.grupo7dh.filmix.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.github.grupo7dh.filmix.MainActivity
+import com.github.grupo7dh.filmix.R
 
 class SpashActivity : AppCompatActivity() {
+    private val splashTimeout: Long = 6000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
         navigateToMainActivity()
     }
     private fun navigateToMainActivity() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, splashTimeout)
     }
 }
